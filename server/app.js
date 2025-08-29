05d17fefcf3c669c17c0bf8baa1b729bb49b8414
Mahin-Touqeer <touqeermahin915@gmail.com>
2025-08-29 14:14:32 +0530

    my first commit

--- server/app.js
+++ server/app.js
@@ -4,14 +4,21 @@ const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
-  console.log("server is running on port 8080");
-  console.log("http://localhost:8080");
+  if ((process.env.NODE_ENV = "development")) {
+    console.log(`server is running on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
+  } else {
+    console.log(`https://hive-19cc.onrender.com`);
+  }
 });
 
 app.get("/", (req, res) => {
+  console.log("hello world");
   res.send("hello world");
 });
+
 app.all("*all", (req, res) => {
   res.status(404).send("not found");
 });
