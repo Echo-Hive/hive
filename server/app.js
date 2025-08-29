2879193c938fbd25eaad3425466db71d1c949381
Apurba-kar <apurbakar1111@gmail.com>
2025-08-29 16:05:18 +0530

    my first commit

--- server/app.js
+++ server/app.js
@@ -1,16 +1,13 @@
 import express from "express";
 const app = express();
+import googleRoutes from "./routes/googleRoutes.js";
 
-app.get("/", (req, res) => {
-  console.log("hello world");
-  res.send("hello world");
-});
+app.use(express.json());
+
+app.use("/", googleRoutes);
 
 app.all("*all", (req, res) => {
   res.status(404).send("not found");
 });
-
-app.use(express.json());
-app.use();
 
 export default app;
--- server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import authController from "../controllers/authController.js";
-
-const router = express.Router();
-
-// Example route for user login
-router.post("/oauth/login", authController.oauthLogin);
-
-export default router;
